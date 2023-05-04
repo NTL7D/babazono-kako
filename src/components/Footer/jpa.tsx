4363a12893b262fd1ddf6667fbaa5d2f5106a03c
NTL7D <nguyenthanhlong528@gmail.com>
2023-05-04 14:06:56 +0700

    jpa footer

--- src/components/Footer/jpa.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const JPAcademyFooter: React.FC = () => {
-    return <div>JPAcademyFooter</div>;
-};
-
-export default JPAcademyFooter;
--- src/layout/jpAcademylayout/index.tsx
+++ src/layout/jpAcademylayout/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { childrenProps } from "../interface";
 import Header from "../../components/Header";
-import JPAcademyFooter from "../../components/Footer/jpa";
 const JPAcademyLayout: React.FC<childrenProps> = ({
     children,
 }): React.ReactElement => {
@@ -9,7 +8,6 @@ const JPAcademyLayout: React.FC<childrenProps> = ({
         <>
             <Header />
             {children}
-            <JPAcademyFooter />
         </>
     );
 };
